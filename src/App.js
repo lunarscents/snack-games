a8b87bafb8478b5bf4a1fed008272a4d9777d101
lunarscent <lunarscent7@gmail.com>
2019-04-14 02:11:25 +0900

    [App, index] - connected semantic-ui-react and made a sample page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Menu, Button, Image, Grid, Card, Icon } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Menu fixed='top' stackable compact>
+          <Menu.Item as='a' header>
+            <Icon name='gamepad' />
+          </Menu.Item>
+
+          <Container>
+
+          </Container>
+        </Menu>
+
+        <Container text style={{ marginTop: '7em' }}>
+          <Grid columns={3} doubling stackable>
+            <Grid.Column>
+              <Card>
+                <Image src='/images/avatar/large/matthew.png' />
+                <Card.Content>
+                  <Card.Header>Multiple</Card.Header>
+                  <Card.Description>Multiple Games.</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <a>
+                    <Icon name='user' />
+                    22 Clicked
+                  </a>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+
+          </Grid>
+        </Container>
       </div>
     );
   }
